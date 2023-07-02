656aedb0ec5105f6cc186479be8d86b84e4652ab
Belen Torres <belentorress@gmail.com>
2023-07-02 19:00:39 -0300

    Modificaciones

--- index.js
+++ index.js
@@ -42,10 +42,67 @@ const onLoad = () => {
     navigator.geolocation.getCurrentPosition(fetchData)
 } */
 
+
+const API_KEY = "750d8443a227b5e847d2c1e53af23b9d"
 const result = document.querySelector(".result")
 const form = document.querySelector(".getWeather")
 const nameCity = document.querySelector("#city")
+const forecast = document.querySelector("#forecast")
 
+const fetchData = position => {
+    const { latitude, longitude } = position.coords;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`)
+        .then(response => response.json())
+        .then(data => setWeatherData(data))
+}
+
+const setWeatherData = (data) => {
+    console.log(data);
+    const weatherData = {
+        icon: "",
+        location: data.name,
+        temperature: Math.round(data.main.temp) + "°C",
+        min: "Min: " + Math.round( data.main.temp_min) + "°C",
+        max: "Max: " + Math.round( data.main.temp_max) + "°C",
+        humidity: "Humedad: " + data.main.humidity + "%",
+        pressure: "Presión: " + data.main.pressure + "hPA",
+    };
+
+    const weatherIcon = data.weather[0].icon;
+    const weatherIconUrl = `https://openweathermap.org/img/w/${weatherIcon}.png`;
+    weatherData.weatherIcon = `<img src="${weatherIconUrl}" alt="Weather Icon" class= "iconGeolocalizado">`;
+
+    Object.keys(weatherData).forEach((key) => {
+        const element = document.getElementById(key);
+        if (element) {
+            if (key === "weatherIcon") {
+                element.innerHTML = weatherData[key];
+            } else {
+                element.textContent = weatherData[key];
+            }
+        }
+    });
+
+    /*     cleanUp() */
+}
+
+
+/* const cleanUp = () => {
+    let container = document.getElementById(`container`)
+    let loader = document.getElementById(`loader`)
+
+    loader.style.display = `none`
+    container.style.display = `flex`
+}
+ */
+/* const getDate = () => {
+    let date = new Date()
+    return `${("0" + date.getDate())}-${("0" + (date.getMonth() + 1)).slice(-2)}-${date.getFullYear()}`
+} */
+
+const onLoad = () => {
+    navigator.geolocation.getCurrentPosition(fetchData)
+}
 
 
 form.addEventListener("submit", (e) => {
@@ -54,12 +111,13 @@ form.addEventListener("submit", (e) => {
         showError("Ambos campos son obligatorios")
         return
     }
+    
 
     callApi(nameCity.value)
 })
 
 function callApi(city) {
-    const API_KEY = "750d8443a227b5e847d2c1e53af23b9d"
+
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},&appid=${API_KEY}`
 
     fetch(url)
@@ -72,6 +130,7 @@ function callApi(city) {
             } else {
                 clearHTML()
                 showWeather(dataJSON)
+                hideGeolocalizado()
             }
         })
         .catch(error => {
@@ -88,9 +147,9 @@ function showWeather(data) {
 
     const content = document.createElement(`div`)
     content.innerHTML = `
-                        <h5>Clima en ${name}</h5>
                         <img src="https://openweathermap.org/img/wn/${arr.icon}@2x.png" class="icono" alt="Icono">
-                        <h2>${degrees}°C</h2>
+                        <h5 class="nombreResultado">${name}</h5>
+                        <h2 class="gradosResultado">${degrees}°C</h2>
                         <p>Min: ${min}°C</p>
                         <p>Max: ${max}°C</p>
                         <p>Humedad: ${humidity}%</p>
@@ -112,10 +171,15 @@ function showError(message) {
     }, 2000)
 }
 
-function kelvinToCentigrade(temp){
-    return parseInt (temp - 273.15)
+function kelvinToCentigrade(temp) {
+    return parseInt(temp - 273.15)
 }
 
-function clearHTML(){
+function clearHTML() {
     result.innerHTML = ""
+}
+
+function hideGeolocalizado(){
+    const geolocalizado = document.getElementById("geolocalizado")
+    geolocalizado.style.display = "none"
 }
